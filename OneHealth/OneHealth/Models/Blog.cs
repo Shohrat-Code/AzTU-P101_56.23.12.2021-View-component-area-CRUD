07a6fb4b6cde8e0d174575767ed6043958df7390
Shohrat-Code <shohrat@code.edu.az>
2021-12-23 16:12:28 +0400

    Codes

---
+++ OneHealth/OneHealth/Models/Blog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(250)]
+        public String Title { get; set; }
+        [MaxLength(250)]
+        public String Image { get; set; }
+        [Column(TypeName = "nText")]
+        public String Content { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreatedDate { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public List<TagToBlog> TagToBlogs { get; set; }
+    }
+}
